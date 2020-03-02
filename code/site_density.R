bd36f862d8b5b4e7d4b834c3b604e3ae1b8ec0a6
Mono <LuciusVorenus@gmx.net>
2020-03-02 17:03:26 +0100

    Calculation of LEC added

--- code/site_density.R
+++ code/site_density.R
@@ -1,4 +1,4 @@
-
+library("magrittr")
 
 #### load data ####
 
@@ -8,21 +8,31 @@ sp::proj4string(sites_spdf) <- sp::CRS("+init=epsg:25832") # !No reprojection, d
 
 #### Largest Empty Circles (LEC) ####
 
-# calculate Thiessen polygons
-Thiessen_polygons <- deldir::deldir(sites_spdf@coords[,1], sites_spdf@coords[,2]) %>%
-  deldir::tile.list()
+# calculate vertices of Thiessen polygons
+Thiessen_vertices <- deldir::deldir(sites_spdf@coords[,1], sites_spdf@coords[,2]) %$%
+  dirsgs
 
-# Thiessen vertices
-Thiessen_vertices <- data.frame(x = unlist(sapply(Thiessen_polygons, "[", "x"),use.names = F),
-                                y = unlist(sapply(Thiessen_polygons, "[", "y"),use.names = F))
+# rearrange Thiessen_vertices in preparation for transformation into an spdf
+Thiessen_vertices <- rbind(setNames(Thiessen_vertices[,c(1:2,5,7,9)], c("x", "y", "ind", "bp", "thirdv")), setNames(Thiessen_vertices[,c(3:4,6,8,10)], c("x", "y", "ind", "bp", "thirdv")))
 
+# transformation of Thiessen_vertices into an spdf
+Thiessen_vertices_spdf <- sp::SpatialPointsDataFrame(coords = Thiessen_vertices[1:2], data = Thiessen_vertices[,3:5] )
+sp::proj4string(Thiessen_vertices_spdf) <- sp::CRS("+init=epsg:25832") # !No reprojection, data is already in EPSG 25832!
 
- 
+# remove dublicates and border points !! see issue for border points!!
+Thiessen_vertices_spdf <- sp::remove.duplicates(Thiessen_vertices_spdf) %>%
+  {.[.[[2]] == FALSE, ]}
+
+# calculate radius of LEC and add this information to Thiessen_vertices_spdf
+Thiessen_vertices_spdf@data$radiusLEC <- rgeos::gDistance(sites_spdf, Thiessen_vertices_spdf, byid = TRUE) %>%
+  apply(1,min)
+  
+
 
 #### needs to be deleted ####
 plot(sites_spdf@coords[,1], sites_spdf@coords[,2], type = "n", asp = 1)
 points(sites_spdf, pch = 20, col = "red")
 points(sites_nodes_spdf, pch = 20, col = "black")
-plot(deldir(sites_spdf@coords[,1], sites_spdf@coords[,2]), wlines = "tess", wpoints = "none", number = FALSE, lty = 1, add = TRUE)
-
+plot(deldir::deldir(sites_spdf@coords[,1], sites_spdf@coords[,2]), wlines = "tess", wpoints = "none", number = FALSE, lty = 1, add = TRUE)
+points(Thiessen_vertices$x, Thiessen_vertices$y)
 
