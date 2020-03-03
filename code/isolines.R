72573af007717e28701aa772732b8dcf4bbf2f24
Mono <LuciusVorenus@gmx.net>
2020-03-03 18:40:04 +0100

    isolines added

--- code/isolines.R
+++ code/isolines.R
@@ -24,7 +24,7 @@ plateau <- gstat::variogram(radiusLEC~1,
   
 # fitting theoretical variogram
 Thiessen_vertices_vario_fit <- gstat::fit.variogram(Thiessen_vertices_vario,
-                                                    vgm(nugget = 0,
+                                                    gstat::vgm(nugget = 0,
                                                         model = "Exp",
                                                         sill = plateau),
                                                     fit.sills = FALSE,
@@ -35,10 +35,10 @@ Thiessen_vertices_vario_fit <- gstat::fit.variogram(Thiessen_vertices_vario,
 # creating a grid for kriging
 grid <- expand.grid(x = seq(as.integer(range(Thiessen_vertices_spdf@coords[,1]))[1],
                             as.integer(range(Thiessen_vertices_spdf@coords[,1]))[2],
-                            by = 1000),
+                            by = 300),
                     y = seq(as.integer(range(Thiessen_vertices_spdf@coords[,2]))[1],
                             as.integer(range(Thiessen_vertices_spdf@coords[,2]))[2],
-                            by = 1000)) %>%
+                            by = 300)) %>%
   {sp::SpatialPoints(coords = .[1:2], proj4string = sp::CRS("+init=epsg:25832"))}
  
 # kriging
@@ -47,7 +47,19 @@ LEC_kriged <- gstat::krige(radiusLEC~1,
                            grid,
                            model = Thiessen_vertices_vario_fit)
 
+#### creating isolines ####
+isoline_polygons <- LEC_kriged %>%
+  {raster::rasterFromXYZ(data.frame(x = sp::coordinates(.)[,1],
+                                  y = sp::coordinates(.)[,2],
+                                  z = .[[1]]),
+                         crs = sites_spdf@proj4string)} %>%
+  as("SpatialGridDataFrame") %>%
+  inlmisc::Grid2Polygons(level = TRUE, at = seq(0,70000,500))
 
 
 
-rm(d1, test)
+
+#### needs to be deleted ####
+
+rgdal::writeOGR(isoline_polygons, dsn = "output", layer = "isoline_polygons", driver = "ESRI Shapefile")
+
