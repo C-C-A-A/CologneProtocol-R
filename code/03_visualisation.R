a63cb9853f46b88274227108636cd9336f7ae7ac
Mono <LuciusVorenus@gmx.net>
2020-03-06 15:15:55 +0100

    minor style and terminology changes

--- code/03_visualisation.R
+++ code/03_visualisation.R
@@ -7,21 +7,25 @@ options(scipen = 999)
 
 # plot of archaeological sites and Thiessen polygons ------------------------
 
-plot(sites_spdf@coords[,1], sites_spdf@coords[,2],
+plot(sites@coords[,1], sites@coords[,2],
      type = "n",
      asp = 1,
-     main = "Sites and corresponding Thiessen polygons",
-     xlab = "[EPSG 25832]",
+     main = "Sites and corresponding voronoi diagram",
+     xlab = your_projection,
      ylab = NA)
-points(sites_spdf, pch = 20, col = "red")
-points(Thiessen_vertices_spdf, pch = 20, col = "black")
-plot(deldir::deldir(sites_spdf@coords[,1], sites_spdf@coords[,2]),
+points(sites,
+       pch = 20,
+       col = "red")
+points(vertices_spdf,
+       pch = 20,
+       col = "black")
+plot(deldir::deldir(sites@coords[,1], sites@coords[,2]),
      wlines = "tess",
      wpoints = "none",
      number = FALSE,
      lty = 1,
      add = TRUE)
-legend(x = 500000, y = 5650000,
+legend(x = "topleft",
        legend = c("Archaeological sites", "Thiessen vertices", "Thiessen polygons"),
        pch = c(20, 20, NA),
        lty = c(NA, NA, 1),
@@ -31,18 +35,26 @@ legend(x = 500000, y = 5650000,
        bg = "transparent")
 
 # variogram / semivariogram ----------------------------------------------------
-plot(Thiessen_vertices_vario, Thiessen_vertices_vario_fit, xlim = c(0,40000), ylim = c(0,50000000))
-plot(Thiessen_vertices_vario, Thiessen_vertices_vario_fit)
+plot(vertices_vario,
+     vertices_vario_fit)
+
+plot(vertices_vario,
+     vertices_vario_fit,
+     xlim = c(0, 40000),
+     ylim = c(0, 50000000))
 
 # Kriging results --------------------------------------------------------------
 LEC_kriged %>%
   as.data.frame() %>%
   ggplot2::ggplot(ggplot2::aes(x = x, y = y)) +
-  ggplot2::geom_tile(ggplot2::aes(fill=var1.pred)) +
+  ggplot2::geom_tile(ggplot2::aes(fill = var1.pred)) +
   ggplot2::coord_equal() +
   ggplot2::scale_fill_gradient2(midpoint = mean(LEC_kriged$var1.pred),
-                                low = "red", mid = "yellow", high = "green") +
-  ggplot2::geom_point(data = as.data.frame(sites_spdf), ggplot2::aes(x = coords.x1, y = coords.x2)) +
+                                low = "red",
+                                mid = "yellow",
+                                high = "green") +
+  ggplot2::geom_point(data = as.data.frame(sites),
+                      ggplot2::aes(x = coords.x1, y = coords.x2)) +
   ggplot2::theme_bw()
 
 # Variance of kriging results --------------------------------------------------
@@ -51,7 +63,10 @@ LEC_kriged %>%
   ggplot2::ggplot(ggplot2::aes(x = x, y = y)) +
   ggplot2::geom_tile(ggplot2::aes(fill = var1.var)) +
   ggplot2::coord_equal() +
-  ggplot2::scale_fill_gradient(low = "yellow", high = "red") +
+  ggplot2::scale_fill_gradient(low = "yellow",
+                               high = "red") +
+  ggplot2::geom_point(data = as.data.frame(sites),
+                      ggplot2::aes(x = coords.x1, y = coords.x2)) +
   ggplot2::theme_bw()
 
 # plot of descriptive properties of isolines -----------------------------------
@@ -64,8 +79,8 @@ Isolines_stats %>%
   ggplot2::labs(title = "Number of distinct areas per Isoline",
                 x = "Isoline [km]",
                 y = "Number of distinct areas") +
-  ggplot2::scale_x_continuous(limit = c(0.5,20),
-                              breaks = seq(0.5,20,0.5)) +
+  ggplot2::scale_x_continuous(limit = c(0.5, 20),
+                              breaks = seq(0.5, 20, 0.5)) +
   ggplot2::theme_bw() +
   ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
                  axis.text.x = ggplot2::element_text(angle = 90))
@@ -79,8 +94,8 @@ Isolines_stats %>%
   ggplot2::labs(title = "Number of sites per Isoline",
                 x = "Isoline [km]",
                 y = "Number of sites") +
-  ggplot2::scale_x_continuous(limit = c(0.5,20),
-                              breaks = seq(0.5,20,0.5)) +
+  ggplot2::scale_x_continuous(limit = c(0.5, 20),
+                              breaks = seq(0.5, 20, 0.5)) +
   ggplot2::theme_bw() +
   ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
                  axis.text.x = ggplot2::element_text(angle = 90))
@@ -93,8 +108,8 @@ Isolines_stats %>%
   ggplot2::labs(title = "Percentage of sites per Isoline",
                 x = "Isoline [km]",
                 y = "Percentage of sites [%]") +
-  ggplot2::scale_x_continuous(limit = c(0.5,20),
-                              breaks = seq(0.5,20,0.5)) +
+  ggplot2::scale_x_continuous(limit = c(0.5, 20),
+                              breaks = seq(0.5, 20, 0.5)) +
   ggplot2::theme_bw() +
   ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
                  axis.text.x = ggplot2::element_text(angle = 90))
@@ -107,8 +122,8 @@ Isolines_stats %>%
   ggplot2::labs(title = "Enclosed area per isoline",
                 x = "Isoline [km]",
                 y = "Area [km²]") +
-  ggplot2::scale_x_continuous(limit = c(0.5,20),
-                              breaks = seq(0.5,20,0.5)) +
+  ggplot2::scale_x_continuous(limit = c(0.5, 20),
+                              breaks = seq(0.5, 20, 0.5)) +
   ggplot2::theme_bw() +
   ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
                  axis.text.x = ggplot2::element_text(angle = 90))
@@ -123,8 +138,8 @@ Isolines_increase %>%
   ggplot2::labs(title = "Increase of number of sites per km",
                 x = "[km]",
                 y = "number of sites") +
-  ggplot2::scale_x_continuous(limit = c(0.5,20),
-                              breaks = seq(0.5,20,0.5)) +
+  ggplot2::scale_x_continuous(limit = c(0.5, 20),
+                              breaks = seq(0.5, 20, 0.5)) +
   ggplot2::theme_bw() +
   ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
                  axis.text.x = ggplot2::element_text(angle = 90))
@@ -137,8 +152,8 @@ Isolines_increase %>%
   ggplot2::labs(title = "Increase of area per km",
                 x = "[km]",
                 y = "Area [km²]") +
-  ggplot2::scale_x_continuous(limit = c(0.5,20),
-                              breaks = seq(0.5,20,0.5)) +
+  ggplot2::scale_x_continuous(limit = c(0.5, 20),
+                              breaks = seq(0.5, 20, 0.5)) +
   ggplot2::theme_bw() +
   ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
                  axis.text.x = ggplot2::element_text(angle = 90))
@@ -153,8 +168,8 @@ Isolines_diff %>%
   ggplot2::labs(title = "Difference of increase of number of sites per km",
                 x = "[km]",
                 y = "Difference") +
-  ggplot2::scale_x_continuous(limit = c(0.5,20),
-                              breaks = seq(0.5,20,0.5)) +
+  ggplot2::scale_x_continuous(limit = c(0.5, 20),
+                              breaks = seq(0.5, 20, 0.5)) +
   ggplot2::theme_bw() +
   ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
                  axis.text.x = ggplot2::element_text(angle = 90))
@@ -167,8 +182,8 @@ Isolines_diff %>%
   ggplot2::labs(title = "Difference of increase of area per km",
                 x = "[km]",
                 y = "Area [km²]") +
-  ggplot2::scale_x_continuous(limit = c(0.5,20),
-                              breaks = seq(0.5,20,0.5)) +
+  ggplot2::scale_x_continuous(limit = c(0.5, 20),
+                              breaks = seq(0.5, 20, 0.5)) +
   ggplot2::theme_bw() +
   ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
                  axis.text.x = ggplot2::element_text(angle = 90))
