448939de7753b673f9d8e051e5a51ebe77fbc477
Mono <LuciusVorenus@gmx.net>
2020-03-06 12:24:06 +0100

    minor style and terminology changes

--- code/01_kriging.R
+++ code/01_kriging.R
@@ -1,67 +1,69 @@
 library("magrittr")
 
-# basics -----------------------------------------------------------------------
+# Basics -----------------------------------------------------------------------
 
-# turn scientific notation off
+# Turn scientific notation off
 options(scipen = 999)
 
-# variogram --------------------------------------------------------------------
+# Variogram --------------------------------------------------------------------
 
-# sample variogram
-Thiessen_vertices_vario <- gstat::variogram(radiusLEC~1,
-                                           Thiessen_vertices_spdf,
-                                           width = sp::spDists(t(Thiessen_vertices_spdf@bbox))[1,2]/250)
+# Sample variogram
+vertices_vario <- gstat::variogram(radiusLEC~1,
+                                   vertices_spdf,
+                                   width = sp::spDists(
+                                     t(vertices_spdf@bbox))[1, 2]/250)
 
-# identify first plateau for fitting theoretical variogram
-range.plateau <- Thiessen_vertices_vario %$%
+# Identify first plateau for fitting theoretical variogram
+range.plateau <- vertices_vario %$%
   gamma %>%
   diff() %>%
-  {Thiessen_vertices_vario[2][which.max(./.[1] < 0.1), ]}
+  {vertices_vario[2][which.max(./.[1] < 0.1), ]}
 
-sill.plateau <- Thiessen_vertices_vario$gamma[Thiessen_vertices_vario$dist == range.plateau]
+sill.plateau <- vertices_vario$gamma[vertices_vario$dist == range.plateau]
   
-# fitting theoretical variogram
-Thiessen_vertices_vario_fit <- gstat::fit.variogram(Thiessen_vertices_vario,
-                                                    gstat::vgm(nugget = 0, # Zimmermann 2004, 52
-                                                        model = "Sph",
-                                                        psill = sill.plateau,
-                                                        range = range.plateau),
-                                                    fit.sills = FALSE,
-                                                    fit.ranges = FALSE)
+# Fitting theoretical variogram
+vertices_vario_fit <- gstat::fit.variogram(vertices_vario,
+                                           # Zimmermann 2004, 52
+                                           gstat::vgm(nugget = 0,
+                                                      model  = your_model,
+                                                      psill  = sill.plateau,
+                                                      range  = range.plateau),
+                                           fit.sills = FALSE,
+                                           fit.ranges = FALSE)
 
-# kriging ----------------------------------------------------------------------
+# Kriging ----------------------------------------------------------------------
 
-# creating a grid for kriging
-grid <- expand.grid(x = seq(as.integer(range(Thiessen_vertices_spdf@coords[,1]))[1],
-                            as.integer(range(Thiessen_vertices_spdf@coords[,1]))[2],
-                            by = 200),
-                    y = seq(as.integer(range(Thiessen_vertices_spdf@coords[,2]))[1],
-                            as.integer(range(Thiessen_vertices_spdf@coords[,2]))[2],
-                            by = 200)) %>%
-  {sp::SpatialPoints(coords = .[1:2], proj4string = sp::CRS("+init=epsg:25832"))}
+# Create a grid for kriging
+grid <- expand.grid(x = seq(as.integer(range(vertices_spdf@coords[, 1]))[1],
+                            as.integer(range(vertices_spdf@coords[, 1]))[2],
+                            by = your_grid_spacing),
+                    y = seq(as.integer(range(vertices_spdf@coords[, 2]))[1],
+                            as.integer(range(vertices_spdf@coords[, 2]))[2],
+                            by = your_grid_spacing)) %>%
+  {sp::SpatialPoints(coords = .[1:2], proj4string = sp::CRS(your_projection))}
  
-# kriging
+# Kriging
 LEC_kriged <- gstat::krige(radiusLEC~1,
-                           Thiessen_vertices_spdf,
+                           vertices_spdf,
                            grid,
-                           model = Thiessen_vertices_vario_fit,
+                           model = vertices_vario_fit,
                            nmin = 3,
                            nmax = 10,
-                           maxdist = sp::spDists(t(Thiessen_vertices_spdf@bbox))[1,2]/2,
+                           maxdist = sp::spDists(t(vertices_spdf@bbox))[1, 2]/2,
                            debug.level = -1)
 
-# creating isolines ------------------------------------------------------------
+# Create isolines ------------------------------------------------------------
 isoline_polygons <- LEC_kriged %>%
-  {raster::rasterFromXYZ(data.frame(x = sp::coordinates(.)[,1],
-                                  y = sp::coordinates(.)[,2],
+  {raster::rasterFromXYZ(data.frame(x = sp::coordinates(.)[, 1],
+                                  y = sp::coordinates(.)[, 2],
                                   z = .[[1]]),
-                         crs = sites_spdf@proj4string)} %>%
+                         crs = sites@proj4string)} %>%
   as("SpatialGridDataFrame") %>%
-  inlmisc::Grid2Polygons(level = TRUE, at = seq(0,70000,500))
+  inlmisc::Grid2Polygons(level = TRUE, at = your_isoline_steps)
 
-# save data --------------------------------------------------------------------
+# Save data --------------------------------------------------------------------
 
-# polygons of isolines as shape file
+# Polygons of isolines as shape file
 rgdal::writeOGR(isoline_polygons,
                 dsn = "output",
                 layer = "isoline_polygons",
