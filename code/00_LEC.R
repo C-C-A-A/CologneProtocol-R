ea7545bc0007afe0427092f7995cdf5452eaf612
RobinCGN <mail2rp@gmx.net>
2020-03-09 14:02:36 +0100

    Added option "remove border points"

--- code/00_LEC.R
+++ code/00_LEC.R
@@ -10,12 +10,13 @@ options(scipen = 999)
 # variables used for loading and creating data 
 your_projection <- "+init=epsg:25832" # the projection of your data
 your_grid_spacing <- 500 # choose some value [m] between 200 and 1000 
-your_isoline_steps <- seq(0, 100000, 1000) # min, max, step
+your_isoline_steps <- seq(0, 5000, 500) # min, max, step (equidistance)
+remove_border_points <- FALSE #Boolean (TRUE/FALSE), wether border points are removed or not
 
 # variables used for kriging
 your_model <- c("Sph") # choose from gstat::show.vgm(), 
-your_nmin <- 3 # minimum number of sites used for interpolation
-your_nmax <- 10 # maximum number of sites used for interpolation
+your_nmin <- 3 # minimum number of sites used for interpolation (your_nmin = 3 is default, Schmidt et al. in prep)
+your_nmax <- 10 # maximum number of sites used for interpolation (your_nmax = 10 is default, Schmidt et al. in prep)
 
 # variables used for plotting statistics of isolines
 # choose according to your_isoline_steps
@@ -64,12 +65,21 @@ vertices_spdf <- sp::SpatialPointsDataFrame(coords = voronoi_vertices[1:2],
 # Ignore warining! It's not a reprojection. But some functions need this step.
 sp::proj4string(vertices_spdf) <- sp::CRS(your_projection) 
 
+
+# If Condition wether border points are removed or not
+if(remove_border_points==TRUE){
 # remove dublicates and border points
 vertices_spdf <- sp::remove.duplicates(vertices_spdf) %>%
   {.[.[[2]] == FALSE, ]}
+} else {
+  # just remove dublicates
+  vertices_spdf <- sp::remove.duplicates(vertices_spdf)  
+}
+
 
 # calculate radius of LEC and add this information to Thiessen_vertices_spdf
 vertices_spdf@data$radiusLEC <- rgeos::gDistance(sites,
                                                  vertices_spdf,
                                                  byid = TRUE) %>%
   apply(1, min)
+
