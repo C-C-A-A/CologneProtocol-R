3d93cfa5cfe132dd1ee089d51655bf4b602a700d
RobinCGN <mail2rp@gmx.net>
2020-03-10 09:08:50 +0100

    Vectorized most Loops

--- code/02_ODI_selection.R
+++ code/02_ODI_selection.R
@@ -28,31 +28,18 @@ Isolines_stats[, 1] <- isoline_polygons@data[, 1]
 # Isolines_stats[, 1] <- Isolines_stats[, 1] / 1000
 
 # Calculate number of sites within each isoline
-Pt_overlay <- sp::over(sites, isoline_polygons)
-Pt_overlay$ID <- rownames(Pt_overlay)
-sites@data$id <- rownames(sites@data)
-sites@data <- dplyr::left_join(sites@data,
-                                    Pt_overlay, by = c("id" = "ID"))
-
-Isolines_stats[1, 3] <- sum(sites@data$z == Isolines_stats[1, 1])
-
-for (i in 2:length(Isolines_stats[, 3])) {
-  Isolines_stats[i, 3] <- sum(sites@data$z == Isolines_stats[i, 1]) +
-    Isolines_stats[i-1, 3]
-}
+sites_n <- sapply(sp::over(isoline_polygons,sites,returnList = TRUE), nrow)
+Isolines_stats$number_Sites <- cumsum(sites_n)
 
 # Calculate the percentage increase in the nummber of site per isoline
-for (i in 1:length(Isolines_stats[,4])) {
-  Isolines_stats[i, 4] <- (Isolines_stats[i, 3] * 100) / length(sites)
-}
+Isolines_stats$percent_Sites <- (Isolines_stats[, 3] * 100) / length(sites)
+
 
 # Calculate area enclosed by each isoline
-Isolines_stats[1, 5] <- raster::area(isoline_polygons)[1] / 1000000
+iso_area <- raster::area(isoline_polygons)/1000000
+Isolines_stats$Area <- cumsum(iso_area)
 
-for (i in 2:length(Isolines_stats[, 5])) {
-  Isolines_stats[i, 5] <- raster::area(isoline_polygons)[i] / 1000000 + 
-    Isolines_stats[i-1, 5]
-}
+
 
 # Increase of number of sites and area per km ----------------------------------
 
@@ -67,6 +54,13 @@ for (i in 1:length(Isolines_stats[, 1])) {
   Isolines_increase[i, 2] <- (Isolines_stats[i+1, 3] - Isolines_stats[i, 3]) * 2
 }
 
+# Why "* 2"?
+increase_sites_alt <- c(sites_n[-1],NA) *2
+names(increase_sites_alt) <- NULL
+
+all.equal(Isolines_increase$increase_Sites, increase_sites_alt)
+
+
 # insert name of isolines
 Isolines_increase[, 1] <- isoline_polygons@data[, 1]
 
@@ -75,6 +69,14 @@ Isolines_increase[, 1] <- isoline_polygons@data[, 1]
 for (i in 1:length(Isolines_stats[, 1])) {
   Isolines_increase[i, 3] <- (Isolines_stats[i+1, 5] - Isolines_stats[i, 5]) * 2
 }
+
+# Why 2? because of "per km"?
+increase_area_alt <- c(iso_area[-1],NA) *2
+names(increase_area_alt) <- NULL
+
+all.equal(Isolines_increase$increase_Area, increase_area_alt)
+
+
 
 # Difference in increase of number of sites and area per km --------------------
 
@@ -89,6 +91,10 @@ for (i in 1:length(Isolines_increase[, 1])) {
   Isolines_diff[i, 2] <- Isolines_increase[i, 2] - Isolines_increase[i+1, 2]
 }
 
+# Alternativ without for-Loop, Make these absolute numbers with abs()?
+diff_Sites_alt <- c(diff(Isolines_increase$increase_Sites)*-1,NA)
+all.equal(Isolines_diff$diff_Sites, diff_Sites_alt)
+
 # Insert name of isolines
 Isolines_diff[, 1] <- isoline_polygons@data[, 1]
 
@@ -96,6 +102,11 @@ Isolines_diff[, 1] <- isoline_polygons@data[, 1]
 for (i in 1:length(Isolines_increase[, 1])) {
   Isolines_diff[i, 3] <- Isolines_increase[i, 3] - Isolines_increase[i+1, 3]
 }
+
+# Alternativ without for-Loop, Make these absolute numbers with abs()?
+diff_Area_alt <- c(diff(Isolines_increase$increase_Area)*-1,NA)
+all.equal(Isolines_diff$diff_Area, diff_Area_alt)
+
 
 # Change units of all data.frames ----------------------------------------------
 Isolines_stats[, 1] <- Isolines_stats[, 1] / 1000
